782d3e0df277eb095ffe04b9c2d5a102e8f8f4df
oguzbatuhan <oguzbatuhanczl@gmail.com>
2026-09-22 20:23:01 +0300

    Python learning, Package and Modules.

--- HelloProject/main.py
+++ HelloProject/main.py
@@ -1,1 +1,11 @@
+from oguzbatuhanmodule import example_func
+from AnimalPackage import info
+from AnimalPackage.CatPackage import meow
+
 print('Hello, World!')
+
+example_func()
+
+info.info()
+
+meow.speak()
---
+++ HelloProject/oguzbatuhanmodule.py
@@ -0,0 +1,2 @@
+def example_func():
+    print('Oguz Batuhan Module')
